3e3ad7875703ba3cc54a2ff7ebca0e4b7e85d80d
Vid Kotnik <kotnik.vid@gmail.com>
2024-11-30 16:14:14 +0100

    Fix broken test

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,7 +1,11 @@
 module.exports = {
-    collectCoverage: true,
-    coverageDirectory: 'coverage',
-    testEnvironment: 'node',
-    timeout: 30000,
-    testTimeoutMillis: 30000,
+	collectCoverage: true,
+	coverageDirectory: "coverage",
+	testEnvironment: "node",
+	timeout: 30000,
+	testTimeoutMillis: 30000,
+	transform: {
+		"^.+\\.mjs$": "babel-jest",
+	},
+	extensionsToTreatAsEsm: [".ts", ".tsx"], // Include only TypeScript extensions if you're using them.
 };
